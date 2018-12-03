514524dc3498045297e46d775700bc0659d1049c
choe <yirang12@gmail.com>
2018-12-03 17:39:33 -0500

    sign-in

--- appointments.js
+++ appointments.js
@@ -1,8 +1,10 @@
+/*
 var appointments = [
     { studentName: 'Natalie', time: '1:30PM Tuesday, 6th November 2018', subject: 'ECO317', details: 'Hi '},
     { studentName: 'Yirang Choe', time: '3:30PM, Tuesday, 6th November 2018', subject: 'CS101'},
     { studentName: 'Mahitha Kalyani', time: '10:30 PM, Tuesday, 6th November 2018', subject: 'CS476'},
     { studentName: 'Michael Scott', time: '2:30 PM, Tuesday, 6th November 2018', subject: 'BS918'}];
+*/
 
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -11,10 +13,10 @@ for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
-        if (panel.style.maxHeight){
-            panel.style.maxHeight = null;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
         } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
+            panel.style.display = "block";
         }
     });
 }
