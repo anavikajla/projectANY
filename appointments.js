09581cce85a7845228b88fe978a5dfcd203d86ad
choe <yirang12@gmail.com>
2018-12-03 15:36:07 -0500

    sign-in

--- appointments.js
+++ appointments.js
@@ -3,12 +3,3 @@ var appointments = [
     { studentName: 'Yirang Choe', time: '3:30PM, Tuesday, 6th November 2018', subject: 'CS101'},
     { studentName: 'Mahitha Kalyani', time: '10:30 PM, Tuesday, 6th November 2018', subject: 'CS476'}
     { studentName: 'Michael Scott', time: '2:30 PM, Tuesday, 6th November 2018', subject: 'BS918'}];
-
-$('#show').click(function() {
-    $('#info').toggle();
-    var visible = $('#info').is(":visible");
-    if(visible)
-        $('a',this).html('Hide details');
-    else
-        $('a',this).html('Show details');
-});
