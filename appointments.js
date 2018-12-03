8720b45870ee11c8788edfe36583248cde1d85e0
choe <yirang12@gmail.com>
2018-12-03 16:17:58 -0500

    sign-in

--- appointments.js
+++ appointments.js
@@ -1,5 +1,5 @@
 var appointments = [
-    { studentName: 'Natalie', time: '1:30PM Tuesday, 6th November 2018', subject: 'ECO317' details: 'Hi '},
+    { studentName: 'Natalie', time: '1:30PM Tuesday, 6th November 2018', subject: 'ECO317', details: 'Hi '},
     { studentName: 'Yirang Choe', time: '3:30PM, Tuesday, 6th November 2018', subject: 'CS101'},
-    { studentName: 'Mahitha Kalyani', time: '10:30 PM, Tuesday, 6th November 2018', subject: 'CS476'}
+    { studentName: 'Mahitha Kalyani', time: '10:30 PM, Tuesday, 6th November 2018', subject: 'CS476'},
     { studentName: 'Michael Scott', time: '2:30 PM, Tuesday, 6th November 2018', subject: 'BS918'}];
