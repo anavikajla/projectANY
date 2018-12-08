20fb53bc91e9846a30bcba7ab77a75b2467d866a
choe <yirang12@gmail.com>
2018-12-08 16:42:44 -0500

    form validationdxdf

--- createAccount.js
+++ createAccount.js
@@ -519,8 +519,22 @@ ddlCountry.onchange = function () {
         ddlStates.add(new Option("Not in USA"));
     }
 }
-var btn = document.getElementById("btnCreateAccount").onclick(){
-    document.getElementById("account_form").submit();
+
+// ----------image upload----------
+
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            $('#img').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
 }
 
-
+$("#imgInp").change(function() {
+    readURL(this);
+});
