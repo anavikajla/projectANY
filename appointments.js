8563d138853fd8c0cfa5cb7956fa314848cdc79d
choe <yirang12@gmail.com>
2018-12-08 19:03:19 -0500

    form validationdxdf

--- appointments.js
+++ appointments.js
@@ -22,8 +22,18 @@ function handleSubmitClick2() {
 
 document.getElementById('del_btn3').addEventListener('click', handleSubmitClick2, false);
 
+//--------scrolling navbar----------
 
-
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("scroll").style.top = "0";
+    } else {
+        document.getElementById("scroll").style.top = "-80px";
+    }
+    prevScrollpos = currentScrollPos;
+}
 
 
 //
